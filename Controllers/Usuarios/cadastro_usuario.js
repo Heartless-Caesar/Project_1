b7968fe7be0fc02c922a1678a22b2d1af483ef46
caesar <joaovictorcsatu@gmail.com>
2022-07-10 00:09:23 -0300

    Added TODO comment

---
+++ Controllers/Usuarios/cadastro_usuario.js
@@ -0,0 +1,5 @@
+//TODO: Add system users email table for verification on registration,
+//this auxiliary table will be used so that a user's password will be hashed
+//and salted by the string of their choice
+
+//If <provided email> is in aux email table the user will register with their password of choice
