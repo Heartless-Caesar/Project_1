18b7a0e257fddf0806eac5ce12b07a4ac62a2103
caesar <joaovictorcsatu@gmail.com>
2022-07-17 14:49:18 -0300

    Added POST controllers

---
+++ Controllers/Resultados/cadastrar_mussarela.js
@@ -0,0 +1,35 @@
+const { StatusCodes } = require('http-status-codes')
+const { resultado_mussarela } = require('../../Data/models')
+
+const cadastrar_mussarela = async (req, res) => {
+    const {
+        data_da_producao,
+        quantidade_produzida,
+        valor_da_pesagem,
+        valor_medio_por_peca,
+    } = req.body
+
+    try {
+        const novaEntrada = await resultado_mussarela.create({
+            data_da_producao: data_da_producao,
+            quantidade_produzida: quantidade_produzida,
+            valor_da_pesagem: valor_da_pesagem,
+            valor_medio_por_peca: valor_medio_por_peca,
+        })
+
+        return res
+            .status(StatusCodes.CREATED)
+            .json({ msg: 'Entrada criada', entrada: novaEntrada })
+    } catch (error) {
+        const errorMessages = []
+
+        error.errors.forEach((x) => errorMessages.push(x.message))
+
+        return res.status(StatusCodes.BAD_REQUEST).json({
+            msg: 'Algo deu errado ao tentar criar a nova entrada',
+            errors: errorMessages,
+        })
+    }
+}
+
+module.exports = { cadastrar_mussarela }
